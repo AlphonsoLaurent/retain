0094a30112b6f536bab5830d49e7df8bda7a385a
alphonsolaurent <alphonsolaurent@gmail.com>
2019-08-05 23:29:56 -0500

    ajuste

--- src/main/webapp/js/toapply/user.js
+++ src/main/webapp/js/toapply/user.js
@@ -1,30 +1,51 @@
-var User = Object.create({ 
+var User = Object.create({
+	classtyle:["bg-primary","bg-warning","bg-success","bg-danger"],
+	initclass:function(){
+		
+		$( ".move" ).draggable({ cursor: "hand"});
+//		$('.card').draggable({
+//			cursor: "move",
+//			cursor: "pointer",
+//		});
+	},
 	loadUsers:function(){
-		debugger;
+		
 		ObjectRequest.requestByAjaxJSON("https://retain.herokuapp.com/toapply/findallUsers", null, function(response){
 			debugger;
-			
+			var contador = 0;
+			var renglon = 0;
+			var cpintar = 0;
 			$.each(response.lstResponse, function(idx0, elem){
 				$("#usermain").html(elem.nameUser);
-				$("#dinamic").append("<div class='row' id='"+idx0+"'><div>");
 				$.each(elem.categoryListDTO, function(idx1, element){
 					if(idx1==0){
 						$("#categoria").html(element.description);
+						$("#dinamic").append("<div class='row' id='"+renglon+"'><div>");
 					}else{
 						
-						var html ="<div class='col-xl-3 col-sm-6 mb-3'>"+
-			            "<div class='card text-white bg-primary o-hidden h-100'>"+
+						if(cpintar % 3 == 0){ 
+							contador=0;
+							renglon++;
+							$("#dinamic").append("<div class='row' id='"+renglon+"'><div>");
+							
+						}else{
+							contador++;
+						}
+						var html ="<div class='col-xl-3 col-sm-6 mb-3 move' style='cursor:pointer;'>"+
+			            "<div class='card text-white "+User.classtyle[contador]+" o-hidden h-100'>"+
 			              "<div class='card-body'>"+
 			                "<div class='card-body-icon'>"+
-			                  "<i class='fas fa-fw fa-comments'></i>"+
+			                  "<i class='fab fa-leanpub'></i>"+
 			                "</div>"+
-			                "<div class='mr-5'>"+element.description+"!</div>"+
+			                "<div class='mr-5'>"+element.description+"</div>"+
 			              "</div>"+
 			            "</div>"+
 			          "</div>";
-						$("#"+idx0).append(html);
+						$("#"+renglon).append(html);
+						cpintar ++;
 					}
 				});
+				User.initclass();
 			});
 			
 			
@@ -32,6 +53,8 @@ var User = Object.create({
 		}, function(dataError){
 			debugger;
 		}, true);
+		
+		
 	},
 	
 	
